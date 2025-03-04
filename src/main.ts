c75b34f48382deb9d4adee90003720308a670d27
pnaskardev <pnaskardev@gmail.com>
2025-03-05 00:28:38 +0530

    succesfully ingesting stream data

--- src/main.ts
+++ src/main.ts
@@ -10,13 +10,22 @@ const main = async () => {
 
     console.log("HI Deno");
 
+    // custom pipeline
+    const pipeline = [{ $addFields: { newField: "this is an added field!" } }];
+
     // listen to the collection
     const collection = dbClient.db("Stream").collection("stream-collection");
-    const changeStream = collection.watch();
-    changeStream.on("create", (change) => {
-      // process next document
-      console.log("Change Stream has been triggered");
-    });
+    const changeStream = collection.watch(pipeline);
+    changeStream
+      .on("change", (next) => {
+        if (next.operationType === "insert") {
+          console.log("Change Stream has been triggered", next.fullDocument);
+        }
+      })
+      .once("error", (error) => {
+        // handle error
+        console.log(error);
+      });
   } catch (error) {
     console.log(error);
   }
